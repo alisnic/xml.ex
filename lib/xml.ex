46e2582e95a568d7787c62cf134e06bb1c6bd060
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-08-07 20:54:51 +0300

    Fix warnings

--- lib/xml.ex
+++ lib/xml.ex
@@ -1,6 +1,4 @@
 defmodule XML do
-  import Record
-
   def doc(str) do
     {xml, _rest} = :xmerl_scan.string(to_unicode_char_list(str))
     xml
@@ -10,10 +8,10 @@ defmodule XML do
     :xmerl_xpath.string to_char_list(path), document
   end
 
-  def children([h|t]),   do: children(h)
+  def children([h|_t]),   do: children(h)
   def children(element), do: elem(element, 8)
 
-  def content([h|t]),  do: content(h)
+  def content([h|_t]),  do: content(h)
   def content(element) do
     children(element)
     |> Enum.find(fn c -> elem(c,0) == :xmlText end)
@@ -21,14 +19,14 @@ defmodule XML do
     |> to_string
   end
 
-  def attributes([h|t]), do: attributes(h)
+  def attributes([h|_t]), do: attributes(h)
   def attributes(element) do
     element
     |> elem(7)
     |> Enum.map(fn e -> {elem(e,1), to_string(elem(e,8))} end)
   end
 
-  def attribute([h|t], name),   do: attribute(h, name)
+  def attribute([h|_t], name),   do: attribute(h, name)
   def attribute(element, name), do: Dict.get(attributes(element), name)
 
   defp to_unicode_char_list(str) do
